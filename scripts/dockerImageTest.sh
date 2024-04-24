5cc8dad931f2308a3651474f47e02040bf1e26b2
Gilbert Kwan <gkwan@ca.ibm.com>
2024-04-24 15:28:29 -0400

    enable docker image test

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -22,8 +22,7 @@ cat module-securing/pom.xml
 cat module-jwt/pom.xml
 cat module-testcontainers/pom.xml
 
-sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM cp.stg.icr.io/cp/olc/open-liberty-daily:full-java11-openj9-ubi;g" module-kubernetes/Dockerfile
-sed -i "s;RUN features.sh;#RUN features.sh;g" module-kubernetes/Dockerfile
+sed -i "s;FROM icr.io/appcafe/open-liberty:full-java17-openj9-ubi;FROM cp.stg.icr.io/cp/olc/open-liberty-daily:full-java17-openj9-ubi;g" module-kubernetes/Dockerfile
 cat module-kubernetes/Dockerfile
 
 echo "$DOCKER_PASSWORD" | sudo docker login -u "$DOCKER_USERNAME" --password-stdin cp.stg.icr.io
